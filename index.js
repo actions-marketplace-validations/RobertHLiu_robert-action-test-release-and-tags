c6861dc8e3197f0b32bb98087a9434e717a2691d
RobertHLiu <robert@cybeats.com>
2023-05-24 11:10:06 -0400

    v1.0.1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const message = 'Release tag semantic version: v1.0.0'
+const message = 'Release tag semantic version: v1.0.1'
 console.log(message);
